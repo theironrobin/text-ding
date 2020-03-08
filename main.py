8c9c58aa34b033501eb834edfdb2a193d54064d6
purism <purism@ironmouse.attlocal.net>
2020-03-08 00:43:03 -0600

    more prompt sound alert + led support

--- main.py
+++ main.py
@@ -1,61 +1,36 @@
 #!/usr/bin/python3
-# Written by Alex Robinson on 3-3-20
-# Requires vlc to work (sudo apt-get install vlc)
-import sqlite3
+import gi
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
 import os
 from pathlib import Path
 
-# Define global variables
 sound_path = Path("/usr/share/sounds/gnome/default/alerts/")
+led_path = Path("/sys/devices/platform/leds/leds/pinephone:green:user/brightness")
 my_sound = sound_path/"bark.ogg"
 bark = "cvlc --play-and-exit " + str(my_sound)
+led_on =  "echo '1' | sudo tee  " + str(led_path) + " > /dev/null"
+led_off =  "echo '0' | sudo tee  " + str(led_path) + " > /dev/null"
 
-db_path = Path("/home/purism/.purple/chatty/db/chatty-history.db")
-conn = sqlite3.connect(str(db_path))
-cur = conn.cursor()
+def text_ding(account, sender, message, conversation, flags):
+    os.system(led_on)
+    os.system(bark)
+    os.system(bark)
 
-smscount_path = Path("/home/purism/Projects/text-ding/SMScount.txt")
+def read_it(gtkconv):
+    os.system(led_off)
 
-# Define functions
-def get_sms_count():
-    buffer = "select * from chatty_im"
-    cur.execute(buffer)
-    sms_count = len(cur.fetchall()) + 1
-    return str(sms_count)
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+bus = dbus.SessionBus()
 
-def compare_sms_counts(new_sms_count):
-    f = open(str(smscount_path), "r")
-    old_sms_count = f.read()
-    f.close()
-    if (new_sms_count != old_sms_count):
-        # find out if last sms was sent or recieved
-        if (last_message_recieved(new_sms_count)):
-            os.system(bark)
-            os.system(bark)
-        update_SMScount(new_sms_count)
-    return
+bus.add_signal_receiver(text_ding,
+                        dbus_interface="im.pidgin.purple.PurpleInterface",
+                        signal_name="ReceivedImMsg")
 
-# This function finds out if the last sms message in your db was a recieved one,
-# as opposed to a sent one which we will ignore and not cause an alert.
-def last_message_recieved(x):
-    buffer = "select * from chatty_im order by id desc"
-    cur.execute(buffer)
-    # grab the second column which shows status. 1 for recieved -1 for sent.
-    status = cur.fetchone()
-    if str(status[2]) == "1":
-        return True
-    else: 
-        return False
+bus.add_signal_receiver(read_it,
+                        dbus_interface="im.pidgin.purple.PurpleInterface",
+                        signal_name="ConversationDisplayed")                        
 
-def update_SMScount(my_sms_count):
-    f = open(str(smscount_path), "w")
-    sms_count = my_sms_count
-    f.write(sms_count)
-    print("SMScount updated")
-    return
+GLib.MainLoop().run()
 
-compare_sms_counts(get_sms_count())
-
-#print(last_message_recieved())
-
-conn.close()
