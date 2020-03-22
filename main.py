f27e55243e4447a2ca25d12e3fa9b784fd4e7b60
Alex R <m.alexanderrobinson@yahoo.com>
2020-03-22 09:47:04 -0500

    add blue led light for incoming calls

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ import os
 from pathlib import Path
 
 sound_path = Path("/usr/share/sounds/gnome/default/alerts/")
-led_path = Path("/sys/devices/platform/leds/leds/pinephone:green:user/brightness")
+green_led_path = Path("/sys/devices/platform/leds/leds/pinephone:green:user/brightness")
+blue_led_path = Path("/sys/devices/platform/leds/leds/pinephone:blue:user/brightness")
 my_sound = sound_path/"bark.ogg"
 bark = "cvlc --play-and-exit " + str(my_sound)
-led_on =  "echo '1' | sudo tee  " + str(led_path) + " > /dev/null"
-led_off =  "echo '0' | sudo tee  " + str(led_path) + " > /dev/null"
+green_led_on =  "echo '1' | sudo tee  " + str(green_led_path) + " > /dev/null"
+green_led_off =  "echo '0' | sudo tee  " + str(green_led_path) + " > /dev/null"
+blue_led_on = "echo '1' | sudo tee  " + str(blue_led_path) + " > /dev/null"
 ignore_read = False
 
 def text_ding(account, sender, message, conversation, flags):
-    os.system(led_on)
+    os.system(green_led_on)
     os.system(bark)
 
 def delay_led_off(account, sender, x, y):
@@ -25,8 +27,12 @@ def delay_led_off(account, sender, x, y):
 def read_it(gtkconv):
     global ignore_read
     if (ignore_read == False):
-        os.system(led_off)
+        os.system(green_led_off)
     ignore_read = False
+
+def called(x, y, z):
+    if (x=="org.gnome.Mutter.DisplayConfig" and int(y[dbus.String("PowerSaveMode")])==0):
+        os.system(blue_led_on)
 
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 bus = dbus.SessionBus()
@@ -42,5 +48,9 @@ bus.add_signal_receiver(delay_led_off,
 bus.add_signal_receiver(read_it,
                         dbus_interface="im.pidgin.purple.PurpleInterface",
                         signal_name="ConversationDisplayed")                        
+
+bus.add_signal_receiver(called,
+                        dbus_interface="org.freedesktop.DBus.Properties",
+                        signal_name="PropertiesChanged")
 
 GLib.MainLoop().run()
